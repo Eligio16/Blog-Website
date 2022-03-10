24d44b2752bbb76f8f50431802d8e20777a37b99
Eligio Quijada <eligio.eq@gmail.com>
2022-03-09 21:13:24 -0500

    Version 1.0 completa

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const ejs = require("ejs");
 const { response } = require("express");
+var _ = require('lodash')
 let posts = [];
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -23,6 +24,19 @@ app.get('/about', (req, res) => {
   res.render('about', {contenidoAbout: aboutContent});
 })
 
+app.get('/post/:idPost', (req, res) => {
+  var reqIdPost = _.lowerCase(req.params.idPost);
+  
+  posts.forEach((post) => {
+    let postGuardado = _.lowerCase(post.nTitulo);
+
+    if (postGuardado == reqIdPost){
+      res.render('post', {pTitulo: post.nTitulo, pContent: post.nPost})
+    }
+  });
+
+})
+
 app.get('/contact', (req, res) => {
   res.render('contact', {contenidoContact: contactContent});
 })
@@ -43,5 +57,5 @@ app.post('/compose', (req, res) => {
 })
 
 app.listen(3000, function() {
-  console.log("Server started on port 3000");
+  console.log("Conexion remota - PC Juanchito:port 3000");
 });
